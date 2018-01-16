2a429040f3ef18ef91683234f6a5b800a6d4b554
Uyen Minh  Do <minhuyendo@gmail.com>
2018-01-16 09:15:21 +0700

    - add upload feature

--- client/App.js
+++ client/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Admin, Resource, Delete } from 'admin-on-rest';
 import loopbackRestClient, {authClient} from 'aor-loopback';
+import addUploadFeature from './rest/addUploadFeature';
 
 import { UserList } from './users';
 
+const uploadCapableClient = addUploadFeature(loopbackRestClient('/api/v1'));
+
 const App = () => (
-    <Admin restClient={loopbackRestClient('/api/v1')} authClient={authClient('/api/v1/users/login')}>
+    <Admin restClient={uploadCapableClient} authClient={authClient('/api/v1/users/login')}>
         <Resource name="users" list={UserList} options={{label: "Users"}}/>
     </Admin>
 );
